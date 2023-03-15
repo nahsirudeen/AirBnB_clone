8f0575afcbc278d4dfffc010191221d984ade5f4
nahsirudeen <arikewuyonasiru@gmail.com>
2023-03-14 20:29:13 -0700

    task

---
+++ models/amenity.py
@@ -0,0 +1,15 @@
+
+#!/usr/bin/python3
+
+"""This file defines the Amenity Model
+It inherits from the BaseModel
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """The Amenity Model"""
+
+    # Attributes
+    name: str = ""
